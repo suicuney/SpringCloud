992b329e787a50f44b49fd104bc626a1b2499572
qianjh <qianjh@wangsu.com>
2020-01-15 10:44:02 +0800

    authservice

--- authservice/src/main/java/com/xiaofeng/authservice/AuthApplication.java
+++ authservice/src/main/java/com/xiaofeng/authservice/AuthApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableHystrixDashboard
 @EnableHystrix
+@EnableZuulProxy
 public class AuthApplication {
 	public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
